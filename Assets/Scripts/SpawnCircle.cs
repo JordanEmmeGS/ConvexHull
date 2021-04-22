8367ab0c59a662a4a1dbc0e1fe6576f5bda0c4cb
JordanEmmeGS <jordan.emme@gravitysketch.com>
2021-04-22 15:07:42 +0100

    ConvexHull in 3D
    Bit broken, not sure if due to near coplanarity, edge cases, or very poor implementation and running out of memory.

--- Assets/Scripts/SpawnCircle.cs
+++ Assets/Scripts/SpawnCircle.cs
@@ -3,13 +3,102 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class ConvexHullConstructor
+public class ConvexHull
 {
     private Vector3 interiorPoint = new Vector3();
-    public (List<Vector3>, List<List<Vector3>>) FindExtremalPoints(List<Vector3> vertices, List<List<Vector3>> edges)
+    private List<Vector3> vertices = new List<Vector3>();
+    private List<List<Vector3>> neighbors = new List<List<Vector3>>();
+    private List<Vector3[]> faces = new List<Vector3[]>();
+
+    public List<Vector3> Vertices
+    {
+        get { return vertices; }
+    }
+
+    public List<Vector3[]> Faces
+    {
+        get { return faces; }
+    }
+
+    public void UpdateHull(Vector3 point)
+    {
+        if (vertices.Count < 3)
+        {
+            vertices.Add(point);
+        }
+
+        else if (vertices.Count == 3)
+        {
+            vertices.Add(point);
+            if (!AreCoplanar(vertices))
+            {
+                InitialTetrahedron();
+            }
+        }
+
+        else
+        {
+            if (!IsInHull(point))
+            {
+                List<Vector3> tangentVertices = new List<Vector3>();
+                List<Vector3> invisibleVertices = new List<Vector3>();
+                
+                for (int i = 0; i < vertices.Count; i++)
+                {
+                    string visibility = VertexVisibility(vertices[i], point);
+                    if (visibility == "tangent")
+                    {
+                        tangentVertices.Add(vertices[i]);
+                    }
+                    else if (visibility == "invisible")
+                    {
+                        invisibleVertices.Add(vertices[i]);
+                    }
+                }
+
+                for (int i = 0; i < invisibleVertices.Count; i++)
+                {
+                    DeleteHullVertex(invisibleVertices[i]);
+                }
+
+                vertices.Add(point);
+                int newPointIndex = vertices.Count - 1;
+
+                neighbors.Add(tangentVertices);
+
+                Vector3 initialVertex = tangentVertices[0];
+                Vector3 toTreatVertex = tangentVertices[0];
+                tangentVertices.Remove(toTreatVertex);
+                
+                while (tangentVertices.Count > 0)
+                {
+                    int vertexIndex = vertices.FindIndex(element => element == toTreatVertex);
+                    List<Vector3> vertexNeighbors = neighbors[vertexIndex];
+                    
+                    for (int i = 0; i < vertexNeighbors.Count; i++)
+                    {
+                        if (tangentVertices.Contains(vertexNeighbors[i]))
+                        {
+                            Vector3[] newFace = new Vector3[] {toTreatVertex, vertexNeighbors[i], point};
+                            faces.Add(newFace);
+                            toTreatVertex = vertexNeighbors[i];
+                            tangentVertices.Remove(toTreatVertex);
+                            break;
+                        }
+
+                        neighbors[vertexIndex].Add(point);
+                    }
+                }
+
+                Vector3[] finalFace = new Vector3[] {toTreatVertex, initialVertex, point};
+
+            }
+        }
+    }
+    public (List<Vector3>, List<Vector3[]>) FindExtremalPoints(List<Vector3> vertices, List<Vector3[]> edges)
     {
         List<Vector3> extPoints = new List<Vector3>();
-        List<List<Vector3>> extEdges = new List<List<Vector3>>();
+        List<Vector3[]> extEdges = new List<Vector3[]>();
         
         if (vertices.Count == 4 && !AreCoplanar(vertices))
         {
@@ -34,37 +123,101 @@ public class ConvexHullConstructor
     }
 
 
-    public (List<Vector3>, List<List<Vector3>>) InitialTetrahedron(List<Vector3> vertices)
+    private void InitialTetrahedron()
     {
-        List<Vector3> extPoints = new List<Vector3>();
-        List<List<Vector3>> extEdges = new List<List<Vector3>>();
-
-        if (vertices.Count == 4 && !AreCoplanar(vertices))
+        for (int i = 0; i < 4; i++)
         {
-            extPoints = vertices;
-            for (int i = 0; i < 4; i++)
+            interiorPoint += vertices[i];
+            Vector3[] triangle = {vertices[(i + 1) % 4], vertices[(i + 2) % 4], vertices[(i + 3) % 4]};
+            faces.Add(triangle);
+            List<Vector3> vertexNeighbor = new List<Vector3>
+                {vertices[(i + 1) % 4], vertices[(i + 2) % 4], vertices[(i + 3) % 4]};
+            neighbors.Add(vertexNeighbor);
+        }
+        interiorPoint = interiorPoint / 4;
+    }
+
+    private bool IsUnderPlane(Vector3 point, Vector3[] triangle)
+    {
+        Vector3 normal = OutwardNormal(triangle);
+        return (Vector3.Dot(point - triangle[0], normal) < 0);
+    }
+
+    private bool IsInHull(Vector3 point)
+    {
+        for (int i = 0; i < faces.Count; i++)
+        {
+            Vector3[] triangle = faces[i];
+            if (!IsUnderPlane(point, triangle))
             {
-                interiorPoint += vertices[i];
-                extEdges.Add(new List<Vector3>());
-                for (int j = 0; j < 4; j++)
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    private string VertexVisibility(Vector3 vertex, Vector3 source)
+    {
+        string visibility = "unknown";
+        for (int i = 0; i < faces.Count; i++)
+        {
+            Vector3[] triangle = faces[i];
+            if (triangle[0] == vertex || triangle[1] == vertex || triangle[2] == vertex)
+            {
+                if (!IsUnderPlane(source, triangle))
                 {
-                    if (i != j)
+                    if (visibility == "unknown")
                     {
-                        extEdges[i].Add(vertices[j]);
+                        visibility = "invisible";
+                    }
+                    else if (visibility == "visible")
+                    {
+                        visibility = "tangent";
+                        break;
+                    }
+                }
+                else
+                {
+                    if (visibility == "unknown")
+                    {
+                        visibility = "visible";
+                    }
+                    else if (visibility == "invisible")
+                    {
+                        visibility = "tangent";
+                        break;
                     }
                 }
             }
         }
-        interiorPoint = interiorPoint / 4;
-        return (extPoints, extEdges);
+        return visibility;
     }
 
-    private void IsInHull()
+    private void DeleteHullVertex(Vector3 vertex)
     {
+        int vertexIndex = vertices.FindIndex(element => element == vertex);
         
+        for (int i = 0; i < neighbors[vertexIndex].Count; i++)
+        {
+            Vector3 neighbor = neighbors[vertexIndex][i];
+            int neighborIndex = vertices.FindIndex(element => element == vertex);
+            neighbors[neighborIndex].Remove(vertex);
+        }
+        
+        neighbors.RemoveAt(vertexIndex);
+        vertices.Remove(vertex);
+        
+        for (int i = 0; i < faces.Count; i++)
+        {
+            Vector3[] triangle = faces[i];
+            if (triangle[0] == vertex || triangle[1] == vertex || triangle[2] == vertex)
+            {
+                faces.RemoveAt(i);
+            }
+        }
     }
 
-    private Vector3 GetOutwardNormal(List<Vector3> triangle)
+    private Vector3 OutwardNormal(Vector3[] triangle)
     {
         Vector3 v1 = triangle[1] - triangle[0];
         Vector3 v2 = triangle[2] - triangle[0];
@@ -78,11 +231,6 @@ public class ConvexHullConstructor
             return normal;
         }
     }
-    
-    private void FindTangent()
-    {
-        
-    }
 
     private bool AreCoplanar(List<Vector3> points)
     {
@@ -93,10 +241,6 @@ public class ConvexHullConstructor
             - col1.y * (col2.x * col3.z - col3.x * col2.z) 
             + col1.z * (col2.x * col3.y - col3.x * col2.y) == 0);
     }
-    
-    
-    
-    
     
     /*public List<Vector2> FindExtremalPoints(List<Vector2> coordinates)
     {
@@ -152,6 +296,7 @@ public class ConvexHullConstructor
     }*/
     
     private bool RightTurn(Vector2 v1, Vector2 v2, Vector2 v3)
+    
     {
         return ((v3.x - v2.x) * (v2.y - v1.y) - (v3.y - v2.y) * (v2.x - v1.x) < 0);
     }
@@ -161,13 +306,13 @@ public class SpawnCircle : MonoBehaviour
     public GameObject extremalPoint;
     public GameObject userPoint;
     
-    private List<Vector3> pointsCoordinates = new List<Vector3>();
+    /*private List<Vector3> pointsCoordinates = new List<Vector3>();
     private List<Vector3> hullVertices = new List<Vector3>();
-    private List<List<Vector3>> hullEdges = new List<List<Vector3>>();
+    private List<Vector3[]> hullFaces = new List<Vector3[]>();*/
     
-    private List<GameObject> gameExtPoints = new List<GameObject>();
+    private List<GameObject> extremalSpheres = new List<GameObject>();
     
-    private ConvexHullConstructor convexHull = new ConvexHullConstructor();
+    private ConvexHull convexHull = new ConvexHull();
     
     void Start()
     {
@@ -180,13 +325,13 @@ public class SpawnCircle : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            SpawnOnClick();
-            (hullVertices, hullEdges) = convexHull.FindExtremalPoints(pointsCoordinates, hullEdges);
-            UpdateExtremal(hullVertices, hullEdges);
+            Vector3 point = SpawnOnClick();
+            convexHull.UpdateHull(point);
+            UpdateExtremalSpheresRender();
         }
     }
 
-    private void SpawnOnClick()
+    private Vector3 SpawnOnClick()
     {
         Vector3 cursorPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f));
 
@@ -194,7 +339,9 @@ public class SpawnCircle : MonoBehaviour
 
         point.transform.position = cursorPos;
 
-        if (pointsCoordinates.Count == 0)
+        return (cursorPos);
+
+        /*if (pointsCoordinates.Count == 0)
         {
             pointsCoordinates.Add(cursorPos);
         }
@@ -209,10 +356,10 @@ public class SpawnCircle : MonoBehaviour
             }
 
             pointsCoordinates.Insert(j, cursorPos);
-        }
+        }*/
     }
     
-    private int Lexicographic(Vector2 v1, Vector2 v2)
+    /*private int Lexicographic(Vector2 v1, Vector2 v2)
     {
         if (v1.x > v2.x)
         {
@@ -237,22 +384,22 @@ public class SpawnCircle : MonoBehaviour
         {
             return -1;
         }
-    }
+    }*/
     
-    private void UpdateExtremal(List<Vector3> vertices,  List<List<Vector3>> edges) 
+    private void UpdateExtremalSpheresRender() 
     {
-        for(int i = 0; i < gameExtPoints.Count; i++)
+        for(int i = 0; i < extremalSpheres.Count; i++)
         {
-            Destroy(gameExtPoints[i]);
+            Destroy(extremalSpheres[i]);
         }
 
-        gameExtPoints = new List<GameObject>();
+        extremalSpheres = new List<GameObject>();
 
-        for (int i = 0; i < vertices.Count; i++)
+        for (int i = 0; i < convexHull.Vertices.Count; i++)
         {
             GameObject point = Instantiate(extremalPoint) as GameObject;
-            point.transform.position = vertices[i];
-            gameExtPoints.Add(point);
+            point.transform.position = convexHull.Vertices[i];
+            extremalSpheres.Add(point);
         }
 
         /*if (coordinates.Count >= 2)
